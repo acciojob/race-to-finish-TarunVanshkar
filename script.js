eee2edb315cfeb8cf5e900d01ebb259472beea61
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-03 22:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+const random = Math.floor(Math.random() * 5) + 1;     // +1 to start it with 1, not with 0
+console.log(random)
+
 const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("promise1")
-    }, 1000);
+        reject("promise1")
+    }, random * 1000);
 })
 
 const promise2 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("promise2")
-    }, 2000);
+        reject("promise2")
+    }, random * 1000);
 })
 
 const promise3 = new Promise((resolve, reject) => {
     setTimeout(() => {
         reject("promise3")
-    }, 3000);
+    }, random * 1000);
 })
 
 const promise4 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("promise4")
-    }, 4000);
+    }, random * 1000);
 })
 
 const promise5 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("promise5")
-    }, 5000);
+    }, random * 1000);
 })
 
 const promises = [promise1, promise2, promise3, promise4, promise5];
